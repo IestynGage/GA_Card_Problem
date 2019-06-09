9556fc1801cb2c120f70e2ad0dda7d5d816be146
IestynGage <33298011+IestynGage@users.noreply.github.com>
2019-06-09 01:03:34 +0100

    Add files via upload
    Added Comments

--- Card_Problem/src/Chromosome/Chromosome.java
+++ Card_Problem/src/Chromosome/Chromosome.java
@@ -2,11 +2,38 @@ package Chromosome;
 
 import java.util.Random;
 
+/**
+ * Chromosome
+ * This class represents one possible solution to the card problem
+ * Each position in the pileType Array determines what card number is in each pile.
+ * (e.g. pos 0 = Addition means card 1 is in Addition Pile)
+ *
+ * @author Iestyn Gage
+ * @date 08/06/2019
+ * @version 1.0
+ */
 public class Chromosome {
-
-    private PileType[] helix;
+    ///////////////////////
+    //  Constants
+    ///////////////////////
     private static final int HELIX_SIZE = 10;
 
+    ///////////////////////
+    //  Arrays
+    ///////////////////////
+    private PileType[] helix;
+
+    ///////////////////////
+    //  Constructors
+    ///////////////////////
+
+    /**
+     * This construcs a new chromosome with 2 parents with a Two-point crossover
+     *
+     * @param parent1
+     * @param parent2
+     * @param mutation
+     */
     public Chromosome(Chromosome parent1, Chromosome parent2,Boolean mutation){
         helix = new PileType[10];
         setHelixPart(0,2,parent1.getHelix());
@@ -18,16 +45,30 @@ public class Chromosome {
 
     }
 
+    /**
+     * This construcs a Chromosome with specific data
+     * @param theHelix
+     */
     public Chromosome(PileType[] theHelix){
         helix = theHelix;
 
     }
 
+    /**
+     * This construcs a empty chromosome
+     */
     public Chromosome(){
 
     }
 
-    public void initalize(){
+    ///////////////////////
+    //  Methods
+    ///////////////////////
+
+    /**
+     * This fills in helix data with random data
+     */
+    public void initilize(){
         helix = new PileType[10];
         Random random = new Random();
 
@@ -45,17 +86,32 @@ public class Chromosome {
         }
     }
 
+    /**
+     * This copys another part of Chromosome to this chromosome
+     *
+     * @param startPositiion
+     * @param endPosition
+     * @param parent
+     */
     private void setHelixPart(int startPositiion, int endPosition, PileType[] parent){
         for(int arrayPosition = startPositiion; arrayPosition<=endPosition; arrayPosition++){
             helix[arrayPosition] = parent[arrayPosition];
         }
     }
 
-
+    /**
+     * This returns the PileType Array
+     *
+     * @return
+     */
     public PileType[] getHelix(){
         return helix;
     }
 
+    /**
+     * This gets the total multiplication number from multplication pile
+     * @return integer value from all multplication pile cards multilpied togeather
+     */
     public int getMultiplication(){
         int output = 1;
         for (int i = 0;i< HELIX_SIZE;i++) {
@@ -66,6 +122,10 @@ public class Chromosome {
         return output;
     }
 
+    /**
+     * This gets the total Addition number from Addition pile
+     * @return integer value from all Addition pile cards added togeather
+     */
     public int getAddition(){
         int output = 0;
         for (int i = 0;i< HELIX_SIZE;i++) {
@@ -76,6 +136,9 @@ public class Chromosome {
         return output;
     }
 
+    /**
+     * This selects random amount of random position in the array to swap the PileTypes
+     */
     public void mutate(){
         Random random = new Random();
 
@@ -92,6 +155,10 @@ public class Chromosome {
 
     }
 
+    /**
+     * This calculates the score from the chromosome.
+     * @return the Score of chromosome
+     */
     public int fitnessFunction(){
         int output;
         int multiplicationPile = (360 - getMultiplication())/10;
@@ -107,6 +174,10 @@ public class Chromosome {
         return output;
     }
 
+    /**
+     * This gets PileType and returns array in string format
+     * @return PileType array in Scring format
+     */
     public String helixToString(){
         StringBuilder output = new StringBuilder();
 
@@ -117,6 +188,12 @@ public class Chromosome {
 
         return output.toString();
     }
+
+    /**
+     * This returns all information in the chromosome
+     *
+     * @return String of all the information in the Chromosome
+     */
     public String toString(){
         String output = new String();
         output = "Helix: " + helixToString() + "\n" + "Multiplication: " + getMultiplication() + " Addition: " + getAddition() + " Fitness Score:" + fitnessFunction();
--- Card_Problem/src/Chromosome/ChromosomeFactory.java
+++ Card_Problem/src/Chromosome/ChromosomeFactory.java
@@ -3,26 +3,57 @@ package Chromosome;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * ChromosomeFactory
+ * This class manages the population (array of chromosomes) and allows for the population to be manipulated
+ *
+ * @author Iestyn Gage
+ * @date 08/06/2019
+ * @version 1.0
+ */
 public class ChromosomeFactory {
-
-    private int generation;
-
-    private ArrayList<Chromosome> population = new ArrayList<Chromosome>();
+    ///////////////////////
+    //  Constants
+    ///////////////////////
     private final static int POPULATION_SIZE = 30;
+    ///////////////////////
+    //  Variables
+    ///////////////////////
+    private int generation;
+    private int chromosomeKilled = 0;
+    ///////////////////////
+    //  ArrayLists
+    ///////////////////////
+    private ArrayList<Chromosome> population = new ArrayList<Chromosome>();
 
+    ///////////////////////
+    //  Constructors
+    ///////////////////////
     public ChromosomeFactory(){
         generation = 0;
         initializePopulation();
     }
 
+    ///////////////////////
+    //  Methods
+    ///////////////////////
+
+    /**
+     * This creates the initial population, it uses POPULATION SIZE to create amount thats needed
+     */
     private void initializePopulation(){
         for (int i = 0; i<POPULATION_SIZE; i++){
             Chromosome toBeAdded = new Chromosome();
-            toBeAdded.initalize();
+            toBeAdded.initilize();
             population.add(toBeAdded);
         }
     }
 
+    /**
+     * This looks through all the solutions in the program to find the best Chromosome to return
+     *
+     * @return best Chromosome in population
+     */
     public Chromosome getBestSolution(){
         Chromosome bestSolution = null;
         for (Chromosome possibleBest : population) {
@@ -34,21 +65,46 @@ public class ChromosomeFactory {
         return bestSolution;
     }
 
+    /**
+     *This grabs a random solution from the population
+     * @return a random Chromosome in the population
+     */
     private Chromosome getRandomSolution(){
         Random random = new Random();
 
         return population.get(random.nextInt(population.size()-1));
     }
 
+    /**
+     * This returns the Chromosome Arraylist which represents population
+     *
+     * @return the population
+     */
     public ArrayList<Chromosome> getPopulation(){
 
         return population;
     }
 
+    /**
+     * This returns the generation integer
+     * @return gengeration integer
+     */
     public int getGeneration(){
         return generation;
     }
 
+    /**
+     * This returns the chromosome discarded by ChromosomeFactory
+     * @return
+     */
+    public int getChromosomeKilled() {
+        return chromosomeKilled;
+    }
+
+    /**
+     * This selects the top ten chromosomes, and ten random chromosomes for next generation. it then discards the remaining
+     * chromosomes.
+     */
     public void discard(){
        ArrayList<Chromosome> startOfNextPopulation = new ArrayList<Chromosome>();
        for(int i=0;i<=10; i++){
@@ -62,10 +118,14 @@ public class ChromosomeFactory {
             population.remove(toBeAdded);
         }
 
+        chromosomeKilled += population.size();
         population = startOfNextPopulation;
 
     }
 
+    /**
+     * This recombines the remaing population untill it reaches the population size.
+     */
     public void reCombination(){
         int amountNeededToBeAdded = POPULATION_SIZE - population.size();
         Random random = new Random();
@@ -79,12 +139,13 @@ public class ChromosomeFactory {
         }
     }
 
+    /**
+     *  This discards the generation and then recombined them to make the next generation
+     */
     public void nextGeneration(){
         generation++;
         discard();
         reCombination();
 
     }
-
-
 }
